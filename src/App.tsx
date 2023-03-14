ca7770f68e857d970a00da02fbc4e2fa113bcefa
kristoferdei <erdeikristof@t-online.hu>
2023-03-14 14:24:58 +0100

    make the implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import MovingHighlight from './components/MovingHighlight'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  background: black;
+  height: 100vh;
+  padding: 20px;
+`
 
 function App() {
   return (
-    <div>
+    <StyledDiv>
       <MovingHighlight />
-    </div>
+    </StyledDiv>
   )
 }
 
--- src/components/MovingHighlight.tsx
+++ src/components/MovingHighlight.tsx
@@ -1,10 +1,51 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState, useRef, MouseEvent } from 'react'
+import styled from 'styled-components'
+
+const Box = styled.div`
+  padding: 12px;
+  color: white;
+  border: 1px solid white;
+  border-radius: 8px;
+`
 
 const MovingHighlight: FunctionComponent = () => {
+  const [bgColor, setBgColor] = useState<string>('black')
+  const [left, setLeft] = useState<number>(0)
+  const [top, setTop] = useState<number>(0)
+
+  const boxRef = useRef<HTMLInputElement>(null)
+
+  const onMouseMoveHandler = (e: MouseEvent<HTMLDivElement>) => {
+    const originalColor = 'black'
+    const lightColor = 'rgb(0,163,255)'
+    const gradientSize = 150
+
+    setLeft(boxRef.current?.offsetLeft ?? 0)
+    const x = e.pageX - left
+
+    setTop(boxRef.current?.offsetTop ?? 0)
+    const y = e.pageY - top
+
+    const xy = x + ' ' + y
+
+    setBgColor(
+      '-webkit-gradient(radial, ' + xy + ', 0, ' + xy + ', ' + gradientSize + ', from(' + lightColor + '), to(rgba(255,255,255,0.0))), ' + originalColor
+    )
+  }
+
+  const onMouseLeaveHandler = () => {
+    setBgColor('black')
+  }
+
   return (
-    <div>
-      <p>Moving highlight</p>
-    </div>
+    <Box style={{ background: bgColor }} ref={boxRef} onMouseMove={(e: MouseEvent<HTMLDivElement>) => onMouseMoveHandler(e)} onMouseLeave={onMouseLeaveHandler}>
+      <h1>Moving highlight</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </Box>
   )
 }
 
