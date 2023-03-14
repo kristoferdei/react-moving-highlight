028945414c244dd2b0d34a162382b2a19d01b515
kristoferdei <erdeikristof@t-online.hu>
2023-03-14 14:03:39 +0100

    create the component

---
+++ src/components/MovingHighlight.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react'
+
+const MovingHighlight: FunctionComponent = () => {
+  return (
+    <div>
+      <p>Moving highlight</p>
+    </div>
+  )
+}
+
+export default MovingHighlight
